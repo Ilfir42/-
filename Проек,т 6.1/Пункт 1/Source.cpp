107d017d290ff9b32ed1c01da25e932a2656b1ed
Ilfir42 <91083775+Ilfir42@users.noreply.github.com>
2021-09-30 16:26:18 +0300

    Delete Проек,т 6.1 directory

--- Проек,т 6.1/Пункт 1/Source.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main()
-{
-	float a, b, c;
-	scanf("%f %f", &a, &b);
-	c = a; a = b; b = c;
-	printf("a=%f b=%f", a, b);
-}
--- Проек,т 6.1/Пункт 2/Source.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main()
-{
-	float a, b, c, d;
-	scanf("%f %f %f", &a, &b, &c);
-	d = b; b = a; a = c; c = d;
-	printf("a=%f b=%f c=%f", a, b, c);
-}
