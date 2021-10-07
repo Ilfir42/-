dee68b849516ac213db0d1b5cd1e61eae1357182
Ilfir42 <91083775+Ilfir42@users.noreply.github.com>
2021-10-07 22:37:21 +0300

    Add files via upload

---
+++ Проект 7/1/Source.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	float rad;
+	scanf("%f", &rad);
+	printf("%f", 3.14 / 180 * rad);
+}
---
+++ Проект 7/2/Source.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	float grad;
+	scanf("%f", &grad);
+	printf("%f", 180 / 3.14 * grad);
+}
